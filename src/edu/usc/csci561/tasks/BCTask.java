56a38422c89cc0758ee61a1e13e09de6d6063572
mohito83 <maggarwa@usc.edu>
2014-04-22 18:15:01 -0700

    Stage II: First draft

--- src/edu/usc/csci561/tasks/BCTask.java
+++ src/edu/usc/csci561/tasks/BCTask.java
@@ -4,7 +4,10 @@
 package edu.usc.csci561.tasks;
 
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 import edu.usc.csci561.HornClause;
 import edu.usc.csci561.IEntailmentTask;
@@ -21,8 +24,107 @@ public class BCTask extends IEntailmentTask {
 
 	@Override
 	public void processEntailment(List<HornClause> clauses, List<String> query) {
-		// TODO Auto-generated method stub
+		StringBuffer buff = new StringBuffer();
+		printLog("<Queue of Goals>#Relevant Rules/Fact#New Goal Introduced");
+		printLog(System.getProperty("line.separator"));
+		for (String q : query) {
+			boolean result = plBCEntailment(clauses, q);
+			buff.append(result ? "YES" : "NO");
+			buff.append(System.getProperty("line.separator"));
 
+			// for the output log files
+			printLog("-------------------------------------------------------------");
+			printLog(System.getProperty("line.separator"));
+		}
+		printResult(buff.toString());
+	}
+
+	/**
+	 * 
+	 * @param clauses
+	 * @param q
+	 * @return
+	 */
+	private boolean plBCEntailment(List<HornClause> clauses, String q) {
+		boolean result = true;
+		Stack<String> agenda = new Stack<String>();
+		agenda.push(q);
+
+		int count = 0;
+		while (!agenda.isEmpty()) {
+			StringBuffer buff = new StringBuffer();
+			String p = agenda.pop();
+
+			if (count > 0 && p.equals(q)) {
+				buff.append(q);
+				buff.append(" # CYCLE DETECTED # N/A");
+				buff.append(System.getProperty("line.separator"));
+				printLog(buff.toString());
+				continue;
+			}
+
+			List<HornClause> matchingCaluseList = getClauseforHead(clauses, p);
+			if (matchingCaluseList.size() == 0) {
+				buff.append(p);
+				buff.append(" # N/A # N/A");
+				buff.append(System.getProperty("line.separator"));
+				printLog(buff.toString());
+				result = false;
+			} else {
+				for (HornClause c : matchingCaluseList) {
+					if (c.getHead() != null) {
+						List<String> body = c.getBody();
+						for (int i = body.size() - 1; i >= 0; i--) {
+							agenda.push(body.get(i));
+						}
+						buff.append(p);
+						buff.append(" # ");
+						buff.append(c.getClauseInfo());
+						buff.append(" # ");
+						int i = 0;
+						for (String s : body) {
+							i++;
+							buff.append(s);
+							if (i < body.size()) {
+								buff.append(", ");
+							}
+						}
+						buff.append(System.getProperty("line.separator"));
+						printLog(buff.toString());
+					} else {
+						buff.append(p);
+						buff.append(" # ");
+						buff.append(p);
+						buff.append(" # N/A");
+						buff.append(System.getProperty("line.separator"));
+						printLog(buff.toString());
+						continue;
+					}
+				}
+			}
+			count++;
+		}
+
+		return result;
+	}
+
+	private List<HornClause> getClauseforHead(List<HornClause> clauses, String q) {
+		List<HornClause> c = new ArrayList<HornClause>();
+		Iterator<HornClause> iter = clauses.iterator();
+		while (iter.hasNext()) {
+			HornClause x = iter.next();
+			if (x.getHead() != null) {
+				if (x.getHead().equals(q)) {
+					c.add(x);
+				}
+			} else {
+				if (x.getBody().contains(q)) {
+					c.add(x);
+				}
+			}
+		}
+
+		return c;
 	}
 
 }
