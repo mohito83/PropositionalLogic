cecb72ac06bd871b9fb672462b5e7eab8953eee4
mohito83 <maggarwa@usc.edu>
2014-04-24 20:00:27 -0700

    StageII: second attempt

--- src/edu/usc/csci561/tasks/BCTask.java
+++ src/edu/usc/csci561/tasks/BCTask.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Stack;
 
 import edu.usc.csci561.HornClause;
 import edu.usc.csci561.IEntailmentTask;
@@ -17,7 +16,6 @@ import edu.usc.csci561.IEntailmentTask;
  * 
  */
 public class BCTask extends IEntailmentTask {
-
 
 	public BCTask(FileWriter logsWriter, FileWriter resultsWriter) {
 		super(logsWriter, resultsWriter);
@@ -29,7 +27,7 @@ public class BCTask extends IEntailmentTask {
 		printLog("<Queue of Goals>#Relevant Rules/Fact#New Goal Introduced");
 		printLog(System.getProperty("line.separator"));
 		for (String q : query) {
-			boolean result = plBCEntailment(clauses, q);
+			boolean result = plBCEntailment(clauses, q, null);
 			buff.append(result ? "YES" : "NO");
 			buff.append(System.getProperty("line.separator"));
 
@@ -46,7 +44,70 @@ public class BCTask extends IEntailmentTask {
 	 * @param q
 	 * @return
 	 */
-	private boolean plBCEntailment(List<HornClause> clauses, String q) {
+	private boolean plBCEntailment(List<HornClause> clauses, String original, String q) {
+		if(q==null){
+			q=original;
+		}else{
+			if(original.equals(q)){
+				StringBuffer buff = new StringBuffer();
+				buff.append(q);
+				buff.append(" # CYCLE DETECTED # N/A");
+				buff.append(System.getProperty("line.separator"));
+				printLog(buff.toString());
+				return false;
+			}
+		}
+		
+		boolean result =false;
+		List<HornClause> matchingClauses = getClausesforHead(clauses, q);
+		if(matchingClauses.size() == 0){
+			StringBuffer buff = new StringBuffer();
+			buff.append(q);
+			buff.append(" # N/A # N/A");
+			buff.append(System.getProperty("line.separator"));
+			printLog(buff.toString());
+			return false;
+		}
+		
+		for(HornClause c : matchingClauses){
+			List<String> pList = c.getBody();
+			if(pList.size() ==1 && pList.get(0).equals(q)){
+				StringBuffer buff = new StringBuffer();
+				buff.append(pList.get(0));
+				buff.append(" # ");
+				buff.append(pList.get(0));
+				buff.append(" # N/A");
+				buff.append(System.getProperty("line.separator"));
+				printLog(buff.toString());
+				result= result|| true;
+			}else{
+				StringBuffer buff = new StringBuffer();
+				buff.append(q);
+				buff.append(" # ");
+				buff.append(c.getClauseInfo());
+				buff.append(" # ");
+				int j = 0;
+				for (String s : pList) {
+					j++;
+					buff.append(s);
+					if (j < pList.size()) {
+						buff.append(", ");
+					}
+				}
+				buff.append(System.getProperty("line.separator"));
+				printLog(buff.toString());
+				for(String s : pList){
+					boolean x = plBCEntailment(clauses, original, s);
+					result = result | x;
+					if(!x){
+						break;
+					}
+				}
+			}
+		}
+		
+		return result;
+		/*
 		boolean result = true;
 		Stack<String> agenda = new Stack<String>();
 		agenda.push(q);
@@ -73,16 +134,16 @@ public class BCTask extends IEntailmentTask {
 				printLog(buff.toString());
 				result = false;
 			} else {
-				 for (int k = matchingClauseList.size() - 1; k >= 0; k--) {
-				 HornClause c = matchingClauseList.get(k);
-				//for (HornClause c : matchingClauseList) {
+				for (int k = matchingClauseList.size() - 1; k >= 0; k--) {
+					HornClause c = matchingClauseList.get(k);
+					// for (HornClause c : matchingClauseList) {
 					buff = new StringBuffer();
 					if (c.getHead() != null) {
 						List<String> body = c.getBody();
 						for (int i = body.size() - 1; i >= 0; i--) {
 							agenda.push(body.get(i));
 						}
-						 //if (k == 0) {
+						// if (k == 0) {
 						buff.append(p);
 						buff.append(" # ");
 						buff.append(c.getClauseInfo());
@@ -97,7 +158,7 @@ public class BCTask extends IEntailmentTask {
 						}
 						buff.append(System.getProperty("line.separator"));
 						printLog(buff.toString());
-						 //}
+						// }
 					} else {
 						buff.append(p);
 						buff.append(" # ");
@@ -113,7 +174,7 @@ public class BCTask extends IEntailmentTask {
 		}
 
 		return result;
-	}
+	*/}
 
 	private List<HornClause> getClausesforHead(List<HornClause> clauses,
 			String q) {
@@ -127,12 +188,43 @@ public class BCTask extends IEntailmentTask {
 				}
 			} else {
 				if (x.getBody().contains(q)) {
+					c = new ArrayList<HornClause>();
 					c.add(x);
+					return c;
 				}
 			}
 		}
 
 		return c;
+	}
+
+}
+
+class HornClauseWrapper {
+	private HornClause clause;
+	private String premise;
+
+	public HornClauseWrapper() {
+		super();
+	}
+
+	public HornClauseWrapper(HornClause c, String p) {
+		this.clause = c;
+		this.premise = p;
+	}
+
+	/**
+	 * @return the clause
+	 */
+	public HornClause getClause() {
+		return clause;
+	}
+
+	/**
+	 * @return the premise
+	 */
+	public String getPremise() {
+		return premise;
 	}
 
 }
