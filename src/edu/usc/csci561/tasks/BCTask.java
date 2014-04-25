d2073e545e79f78f534428271801366b19f21b0e
mohito83 <maggarwa@usc.edu>
2014-04-25 10:58:00 -0700

    StageII: Second try v 0.2

--- src/edu/usc/csci561/tasks/BCTask.java
+++ src/edu/usc/csci561/tasks/BCTask.java
@@ -44,11 +44,12 @@ public class BCTask extends IEntailmentTask {
 	 * @param q
 	 * @return
 	 */
-	private boolean plBCEntailment(List<HornClause> clauses, String original, String q) {
-		if(q==null){
-			q=original;
-		}else{
-			if(original.equals(q)){
+	private boolean plBCEntailment(List<HornClause> clauses, String original,
+			String q) {
+		if (q == null) {
+			q = original;
+		} else {
+			if (original.equals(q)) {
 				StringBuffer buff = new StringBuffer();
 				buff.append(q);
 				buff.append(" # CYCLE DETECTED # N/A");
@@ -57,10 +58,10 @@ public class BCTask extends IEntailmentTask {
 				return false;
 			}
 		}
-		
-		boolean result =false;
+
+		boolean result = false;
 		List<HornClause> matchingClauses = getClausesforHead(clauses, q);
-		if(matchingClauses.size() == 0){
+		if (matchingClauses.size() == 0) {
 			StringBuffer buff = new StringBuffer();
 			buff.append(q);
 			buff.append(" # N/A # N/A");
@@ -68,10 +69,10 @@ public class BCTask extends IEntailmentTask {
 			printLog(buff.toString());
 			return false;
 		}
-		
-		for(HornClause c : matchingClauses){
+
+		for (HornClause c : matchingClauses) {
 			List<String> pList = c.getBody();
-			if(pList.size() ==1 && pList.get(0).equals(q)){
+			if (pList.size() == 1 && pList.get(0).equals(q)) {
 				StringBuffer buff = new StringBuffer();
 				buff.append(pList.get(0));
 				buff.append(" # ");
@@ -79,8 +80,8 @@ public class BCTask extends IEntailmentTask {
 				buff.append(" # N/A");
 				buff.append(System.getProperty("line.separator"));
 				printLog(buff.toString());
-				result= result|| true;
-			}else{
+				result = result | true;
+			} else {
 				StringBuffer buff = new StringBuffer();
 				buff.append(q);
 				buff.append(" # ");
@@ -96,85 +97,19 @@ public class BCTask extends IEntailmentTask {
 				}
 				buff.append(System.getProperty("line.separator"));
 				printLog(buff.toString());
-				for(String s : pList){
+				for (String s : pList) {
 					boolean x = plBCEntailment(clauses, original, s);
-					result = result | x;
-					if(!x){
+					if (!x) {
 						break;
+					}else{
+						result = result | x;
 					}
 				}
 			}
 		}
-		
-		return result;
-		/*
-		boolean result = true;
-		Stack<String> agenda = new Stack<String>();
-		agenda.push(q);
-
-		int count = 0;
-		while (!agenda.isEmpty()) {
-			StringBuffer buff = new StringBuffer();
-			String p = agenda.pop();
-
-			if (count > 0 && p.equals(q)) {
-				buff.append(q);
-				buff.append(" # CYCLE DETECTED # N/A");
-				buff.append(System.getProperty("line.separator"));
-				printLog(buff.toString());
-				continue;
-			}
-
-			List<HornClause> matchingClauseList = getClausesforHead(clauses, p);
-
-			if (matchingClauseList.size() == 0) {
-				buff.append(p);
-				buff.append(" # N/A # N/A");
-				buff.append(System.getProperty("line.separator"));
-				printLog(buff.toString());
-				result = false;
-			} else {
-				for (int k = matchingClauseList.size() - 1; k >= 0; k--) {
-					HornClause c = matchingClauseList.get(k);
-					// for (HornClause c : matchingClauseList) {
-					buff = new StringBuffer();
-					if (c.getHead() != null) {
-						List<String> body = c.getBody();
-						for (int i = body.size() - 1; i >= 0; i--) {
-							agenda.push(body.get(i));
-						}
-						// if (k == 0) {
-						buff.append(p);
-						buff.append(" # ");
-						buff.append(c.getClauseInfo());
-						buff.append(" # ");
-						int j = 0;
-						for (String s : body) {
-							j++;
-							buff.append(s);
-							if (j < body.size()) {
-								buff.append(", ");
-							}
-						}
-						buff.append(System.getProperty("line.separator"));
-						printLog(buff.toString());
-						// }
-					} else {
-						buff.append(p);
-						buff.append(" # ");
-						buff.append(p);
-						buff.append(" # N/A");
-						buff.append(System.getProperty("line.separator"));
-						printLog(buff.toString());
-						continue;
-					}
-				}
-			}
-			count++;
-		}
 
 		return result;
-	*/}
+	}
 
 	private List<HornClause> getClausesforHead(List<HornClause> clauses,
 			String q) {
